b09d0e62f498524c2c089bc8ab759ee2b0ee356f
Katona Márk <katonamark0919@gmail.com>
2026-09-15 09:28:19 +0200

    Add files via upload

---
+++ RandomSzam/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace RandomSzam
+{
+    internal class Program
+    {
+        class Szam
+        {
+            public int Szamok {  get; set; }
+
+
+            public Szam(string sor)
+            {
+                string[] darabok = sor.Split(',');
+                Szamok = int.Parse(darabok[0]);
+
+            }
+        }
+        
+        static void Main(string[] args)
+        {
+
+            Console.Write("1. feladat:"); 
+
+            Random r = new Random();
+            int[] szamok = new int[280];
+
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                szamok[i] = r.Next(1, 1000);
+
+
+                if (szamok[i] % 3 == 0 && szamok[i] % 7 == 0)
+                {
+                    Console.Write($"{szamok[i] + ","} ");
+                }
+            }
+
+
+
+            using (StreamWriter sw = new StreamWriter("szamok.txt"))
+            {
+                foreach (var s in szamok)
+                {
+                    sw.WriteLine(s);
+                }
+            }
+            
+           
+
+
+
+
+
+
+
+
+        }
+    }
+}
